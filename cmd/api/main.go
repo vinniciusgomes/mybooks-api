87480481cf114ca4bb18d05f5dc64c97171b14a4
Vinnicius Gomes <vinniciusgomes@outlook.com.br>
2024-05-29 10:19:19 -0300

    feat(init): add Dockerfile and folders struct

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,14 +2,32 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	e.GET("/health", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Health is OK!!")
+	})
+
+	// Start server
+	httpPort := os.Getenv("PORT")
+	if httpPort == "" {
+		httpPort = "8080"
+	}
+
+	e.Logger.Fatal(e.Start(":" + httpPort))
 }

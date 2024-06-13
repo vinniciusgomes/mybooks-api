0d2aa92d1d3cb0cd17681711743bf9a379d2a1f6
Vinnicius Gomes <vinniciusgomes@outlook.com.br>
2024-06-13 18:11:07 -0300

    feat(forgot-password): add endpoints to send a token and reset password

--- internal/domain/repositories/auth_repository.go
+++ internal/domain/repositories/auth_repository.go
@@ -5,13 +5,18 @@ import (
 	"fmt"
 	"mybooks/internal/domain/models"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type AuthRepository interface {
 	CreateUser(user *models.User) error
+	UpdatePassword(userID uuid.UUID, newPassword string) error
 	GetUserByEmail(email string) (*models.User, error)
 	GetUserByID(id string) (*models.User, error)
+	CreateToken(token *models.ValidationToken) error
+	GetToken(token string) (*models.ValidationToken, error)
+	InvalidateToken(token *models.ValidationToken) error
 }
 
 type authRepositoryImp struct {
@@ -78,4 +83,55 @@ func (r *authRepositoryImp) GetUserByID(id string) (*models.User, error) {
 	}
 
 	return &user, nil
+}
+
+// CreateToken creates a new token in the authentication repository.
+//
+// Parameters:
+// - token: a pointer to a ValidationToken object representing the token to be created.
+//
+// Returns:
+// - error: an error object if there was an issue creating the token, otherwise nil.
+func (r *authRepositoryImp) CreateToken(token *models.ValidationToken) error {
+	if err := r.db.Create(token).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetToken retrieves a token from the database based on the provided token string.
+//
+// Parameters:
+// - token: a string representing the token.
+//
+// Returns:
+// - *models.ValidationToken
+func (r *authRepositoryImp) GetToken(token string) (*models.ValidationToken, error) {
+	var validationToken models.ValidationToken
+
+	if err := r.db.Where("token = ?", token).First(&validationToken).Error; err != nil {
+		return nil, err
+	}
+
+	return &validationToken, nil
+}
+
+// UpdatePassword updates the password of a user in the database based on the provided user ID.
+//
+// - userID: a UUID representing the ID of the user.
+// - newPassword: a string representing the new password to be set.
+// Returns an error object if there was an issue updating the password, otherwise nil.
+func (r *authRepositoryImp) UpdatePassword(userID uuid.UUID, newPassword string) error {
+	return r.db.Model(&models.User{}).Where("id = ?", userID).Update("password", newPassword).Error
+}
+
+// InvalidateToken updates the validity status of a token in the database.
+//
+// Parameters:
+// - token: a pointer to a ValidationToken object to be invalidated.
+// Returns:
+// - error: an error object if there was an issue invalidating the token, otherwise nil.
+func (r *authRepositoryImp) InvalidateToken(token *models.ValidationToken) error {
+	return r.db.Model(token).Where("Token = ?", token.Token).Update("valid", false).Error
 }
